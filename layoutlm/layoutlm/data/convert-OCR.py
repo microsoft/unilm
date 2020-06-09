63912473da0fb63cbe575254b43d742bff14633f
Chris <chris.xu@cedrusco.com>
2020-06-09 13:36:41 -0400

    Simplified call to convert_img_to_xml

--- layoutlm/layoutlm/data/convert-OCR.py
+++ layoutlm/layoutlm/data/convert-OCR.py
@@ -1,18 +1,19 @@
 import os, sys, subprocess, shutil
 
 def convert_all_to_xml(root):
-    out_dir = root + '/out-OCR/'
+    output_dir = root + '/out-OCR/'
     try:
-        os.mkdir(out_dir) # make output directory if it does not exist already
+        os.mkdir(output_dir) # make output directory if it does not exist already
     except:
         pass
     for path, directories, files in os.walk(root):
         for file in files:  # walk through dataset for img files
             if file.endswith('.png') | file.endswith('.jpg'):
-                convert_img_to_xml(path, file, out_dir)
+                img = os.path.join(path, file)
+                convert_img_to_xml(img, output_dir)
 
-def convert_img_to_xml(img_path, img_file, output_dir):
-    img = os.path.join(img_path, img_file)
+def convert_img_to_xml(img, output_dir):
+    img_path, img_file = os.path.split(img)
     output_file = os.path.join(output_dir, os.path.splitext(img_file)[0])
     subprocess.run(['tesseract', img, output_file, 'hocr']) # make call to Tesseract with img file and output dir
     old_file_path = output_file + '.hocr'
