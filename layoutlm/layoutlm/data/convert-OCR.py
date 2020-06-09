4caa7e3602a2fb60bea249d6c245dfd17872b7ab
Chris <chris.xu@cedrusco.com>
2020-06-09 13:31:49 -0400

    Refactored img conversion script to accept single image file

--- layoutlm/layoutlm/data/convert-OCR.py
+++ layoutlm/layoutlm/data/convert-OCR.py
@@ -1,7 +1,7 @@
 import os, sys, subprocess, shutil
 
-def convertImages(root):
-    out_dir = root + "/out-OCR/"
+def convert_all_to_xml(root):
+    out_dir = root + '/out-OCR/'
     try:
         os.mkdir(out_dir) # make output directory if it does not exist already
     except:
@@ -9,20 +9,18 @@ def convertImages(root):
     for path, directories, files in os.walk(root):
         for file in files:  # walk through dataset for img files
             if file.endswith('.png') | file.endswith('.jpg'):
-                img = os.path.join(path, file)
-                out = os.path.join(out_dir, os.path.splitext(file)[0])
-                subprocess.run(
-                    ["tesseract", img, out, "hocr"])  # make call to tesseract with img file and output directory
-    for path, directories, files in os.walk(out_dir):
-        for file in files:  # walk through output directory for .hocr files
-            if file.endswith('.hocr'):
-                base, ext = os.path.splitext(file)
-                old_file_path = os.path.join(path, file)
-                new_file_path = os.path.join(out_dir, base + '.xml')
-                if not os.path.exists(new_file_path):
-                    shutil.copy(old_file_path, new_file_path)  # copy .hocr file to .xml
-                    os.remove(old_file_path)  # delete original .hocr file
+                convert_img_to_xml(path, file, out_dir)
+
+def convert_img_to_xml(img_path, img_file, output_dir):
+    img = os.path.join(img_path, img_file)
+    output_file = os.path.join(output_dir, os.path.splitext(img_file)[0])
+    subprocess.run(['tesseract', img, output_file, 'hocr']) # make call to Tesseract with img file and output dir
+    old_file_path = output_file + '.hocr'
+    new_file_path = output_file + '.xml'
+    if not os.path.exists(new_file_path):
+        shutil.copy(old_file_path, new_file_path) # copy .hocr file contents into .xml file
+        os.remove(old_file_path) # delete original .hocr file
 
 
 if __name__ == "__main__":
-    convertImages(sys.argv[1])
+    convert_all_to_xml(sys.argv[1])
