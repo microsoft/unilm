9c89b2a6a797bd9ddd93aef8a9580b9ea6dea9b4
stanleyran <ran.xu@salesforce.com>
2020-09-04 00:31:48 -0700

    add qa bert class

--- layoutlm/layoutlm/modeling/layoutlm.py
+++ layoutlm/layoutlm/modeling/layoutlm.py
@@ -296,3 +296,66 @@ class LayoutlmForSequenceClassification(BertPreTrainedModel):
             outputs = (loss,) + outputs
 
         return outputs  # (loss), logits, (hidden_states), (attentions)
+
+
+
+class LayoutlmForQuestionAnswerg(BertPreTrainedModel):
+    config_class = LayoutlmConfig
+    pretrained_model_archive_map = LAYOUTLM_PRETRAINED_MODEL_ARCHIVE_MAP
+    base_model_prefix = "bert"
+
+    def __init__(self, config):
+        super().__init__(config)
+        self.bert = LayoutlmModel(config)
+        # predict start and end position for extrastive QA
+        self.qa_outputs = nn.Linear(config.hidden_size, 2)
+        self.init_weights()
+
+    def forward(
+            self,
+            input_ids,
+            bbox,
+            attention_mask=None,
+            token_type_ids=None,
+            position_ids=None,
+            head_mask=None,
+            inputs_embeds=None,
+            start_positions=None,
+            end_positions=None):
+
+        outputs = self.bert(
+            input_ids=input_ids,
+            bbox=bbox,
+            attention_mask=attention_mask,
+            token_type_ids=token_type_ids,
+            position_ids=position_ids,
+            head_mask=head_mask,
+        )
+        sequence_output = outputs[0]
+        logits = self.qa_outputs(sequence_output)
+        start_logits, end_logits = logits.split(1, dim=-1)
+        start_logits = start_logits.squeeze(-1)
+        end_logits = end_logits.squeeze(-1)
+        outputs = (start_logits, end_logits,) + outputs[
+            2:
+        ]  # add hidden states and attention if they are here
+
+        if start_positions is not None and end_positions is not None:
+            # If we are on multi-GPU, split add a dimension
+            if len(start_positions.size()) > 1:
+                start_positions = start_positions.squeeze(-1)
+            if len(end_positions.size()) > 1:
+                end_positions = end_positions.squeeze(-1)
+            # sometimes the start/end positions are outside our model inputs, we ignore these terms
+            ignored_index = start_logits.size(1)
+            start_positions.clamp_(0, ignored_index)
+            end_positions.clamp_(0, ignored_index)
+
+            loss_fct = CrossEntropyLoss(ignore_index=ignored_index)
+            start_loss = loss_fct(start_logits, start_positions)
+            end_loss = loss_fct(end_logits, end_positions)
+            total_loss = (start_loss + end_loss) / 2
+            outputs = (total_loss,) + outputs
+
+        return outputs
+
