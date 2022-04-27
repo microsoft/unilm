925de7a9ea500e992ec5de02ea193a5eb9d5aa26
Yupan Huang <hypjudy@qq.com>
2022-04-27 12:38:56 +0800

    remove an unused function
    https://github.com/microsoft/unilm/issues/699

--- layoutlmv3/examples/object_detection/ditod/rcnn_vl.py
+++ layoutlmv3/examples/object_detection/ditod/rcnn_vl.py
@@ -161,23 +161,3 @@ class VLGeneralizedRCNN(GeneralizedRCNN):
                 )
                 inputs, instances = [], []
         return outputs
-
-    def _merge_detections(self, all_boxes, all_scores, all_classes, shape_hw):
-        # select from the union of all results
-        num_boxes = len(all_boxes)
-        num_classes = 5  # self.cfg.MODEL.ROI_HEADS.NUM_CLASSES
-        # +1 because fast_rcnn_inference expects background scores as well
-        all_scores_2d = torch.zeros(num_boxes, num_classes + 1, device=all_boxes.device)
-        for idx, cls, score in zip(count(), all_classes, all_scores):
-            all_scores_2d[idx, cls] = score
-
-        merged_instances, _ = fast_rcnn_inference_single_image(
-            all_boxes,
-            all_scores_2d,
-            shape_hw,
-            1e-8,
-            0.5,  # self.cfg.MODEL.ROI_HEADS.NMS_THRESH_TEST,
-            100,  # self.cfg.TEST.DETECTIONS_PER_IMAGE,
-        )
-
-        return merged_instances
