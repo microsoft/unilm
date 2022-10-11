dad66d00e52c785b99bcc35e2e94c6d5daf65cfb
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-11 18:53:03 +0000

    Adding tarfile member sanitization to extractall()

--- unilm-v1/src/pytorch_pretrained_bert/modeling.py
+++ unilm-v1/src/pytorch_pretrained_bert/modeling.py
@@ -704,7 +704,26 @@ class PreTrainedBertModel(nn.Module):
             logger.info("extracting archive file {} to temp dir {}".format(
                 resolved_archive_file, tempdir))
             with tarfile.open(resolved_archive_file, 'r:gz') as archive:
-                archive.extractall(tempdir)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(archive, tempdir)
             serialization_dir = tempdir
         # Load config
         if ('config_path' in kwargs) and kwargs['config_path']:
