45270b944f345960f4a0c7479a930b7c92a1af76
Li Dong <donglixp@gmail.com>
2023-11-12 08:53:01 +0800

    Update vl_setup_xl.sh

--- kosmos-2/vl_setup_xl.sh
+++ kosmos-2/vl_setup_xl.sh
@@ -3,5 +3,5 @@ pip install infinibatch/
 pip install torchscale/
 pip install open_clip/
 pip install --user git+https://github.com/microsoft/DeepSpeed.git@jeffra/engine-xthru-v2
-pip install -v -U git+https://github.com/facebookresearch/xformers.git@main#egg=xformers
+pip install -v -U git+https://github.com/facebookresearch/xformers.git@v0.0.22
 pip install numpy==1.23.0 tiktoken ftfy sentencepiece httpcore==0.17.3 gradio==3.37.0 spacy==3.6.0 thinc==8.1.10 pydantic==1.10.11
